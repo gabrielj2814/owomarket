0c4e3d1a0290fa2de067586ffa38e3d58f87039b
Gabriel Valera <gabrielvalera@valdusoft.com>
2025-10-27 20:49:00 -0400

    implementando flowbite

---
+++ resources/js/components/test.tsx
@@ -0,0 +1,16 @@
+import {Button} from 'flowbite-react';
+
+
+const Test = () => {
+
+    return (
+        <>
+        <div className="p-4">
+            <Button>Botón de Flowbite</Button>
+        </div>
+        </>
+    )
+
+}
+
+export default Test;
--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,24 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-             refresh: {
-                // Configuración para detectar cambios en subdominios
-                paths: [
-                    'resources/views/**',
-                    'resources/js/**',
-                    'routes/**',
-                    'lang/**',
-                ],
-            },
-        }),
-        react(),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
+    plugins: [laravel({
+        input: ['resources/css/app.css', 'resources/js/app.tsx'],
+        ssr: 'resources/js/ssr.tsx',
+         refresh: {
+            // Configuración para detectar cambios en subdominios
+            paths: [
+                'resources/views/**',
+                'resources/js/**',
+                'routes/**',
+                'lang/**',
+            ],
+        },
+    }), react(), tailwindcss(), wayfinder({
+        formVariants: true,
+    }), flowbiteReact()],
     esbuild: {
         jsx: 'automatic',
     },
